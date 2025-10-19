26d20bc366e213a65d23fd1c858fef9bfa9997f7
Angell2900 <Angelihimbazwe29@gmail.com>
2025-10-19 19:18:25 +0200

    populating database

---
+++ task_5.sql
@@ -0,0 +1,7 @@
+USE alx_book_store;
+
+INSERT INTO Customers (customer_name, email, address)
+VALUES
+('John Doe', 'john@example.com', '123 Main St'),
+('Jane Smith', 'jane@example.com', '456 Market Ave'),
+('Michael Brown', 'michael@example.com', '789 Broad St');
