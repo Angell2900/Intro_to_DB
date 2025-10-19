294fe1c4970b9be8e019a37a2534372d9f2b116c
Angell2900 <Angelihimbazwe29@gmail.com>
2025-10-19 20:13:54 +0200

    inserted book data

---
+++ task_6.sql
@@ -0,0 +1,8 @@
+USE alx_book_store;
+
+INSERT INTO Books (title, author_id, price, publication_date) VALUES
+('Things Fall Apart', 1, 15.99, '1958-06-17'),
+('Harry Potter and the Philosopher''s Stone', 2, 29.99, '1997-06-26'),
+('1984', 3, 19.99, '1949-06-08'),
+('Wizard of the Crow', 4, 18.50, '2006-01-01'),
+('Animal Farm', 3, 12.99, '1945-08-17');
