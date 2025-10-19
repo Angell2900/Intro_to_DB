1dd64d0e25d6051b91a6e7b1a58bc0aec014cbeb
Angell2900 <Angelihimbazwe29@gmail.com>
2025-10-19 19:23:23 +0200

    final edits

--- task_5.sql
+++ task_5.sql
@@ -4,4 +4,5 @@ INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES
 (1, 'John Doe', 'john@example.com', '123 Main St'),
 (2, 'Jane Smith', 'jane@example.com', '456 Market Ave'),
-(3, 'Michael Brown', 'michael@example.com', '789 Broad St');
+(3, 'Michael Brown', 'michael@example.com', '789 Broad St'),
+(4, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
